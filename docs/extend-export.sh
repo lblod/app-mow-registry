adb4d6d776774e0dde713f6cdfb836b7eb9a6ac5
Boris De Vloed <boris.de.vloed@gmail.com>
2021-09-24 09:38:11 +0200

    space

--- docs/extend-export.sh
+++ docs/extend-export.sh
@@ -3,5 +3,5 @@ if [ -z "$1" ]
 then
   echo "Expecting ttl file with exported data as argument"
 else
-  docker run --rm  -v $(pwd):$(pwd) -w $(pwd) bdevloed/eye extend-export.n3 ${1} --nope > extended_export.ttl
+  docker run --rm -v $(pwd):$(pwd) -w $(pwd) bdevloed/eye extend-export.n3 ${1} --nope > extended_export.ttl
 fi
