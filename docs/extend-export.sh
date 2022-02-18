f931390e68070e6e1ee701c92f4bc4fc7191f91c
Sergey Andreev <sergey.andreev@redpencil.io>
2022-02-18 16:59:41 +0100

    most things fixed

--- docs/extend-export.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Expecting ttl file with exported data as argument"
-else
-  docker run --rm -v $(pwd):$(pwd) -w $(pwd) bdevloed/eye extend-export.n3 ${1} --nope > extended_export.ttl
-fi
