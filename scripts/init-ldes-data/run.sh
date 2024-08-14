6cfdd61cbfc2fa33dbd5c7be27d24c75e5db5463
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2024-08-14 14:18:42 +0200

    update comment

--- scripts/init-ldes-data/run.sh
+++ scripts/init-ldes-data/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /data/app/data/files
 npm i
-# we bypass mu auth to avoid producing delta messages that could be consumed by the ldes consumer
+# we bypass mu auth to be able to get the result of our construct query in n-triples format
 MU_SPARQL_ENDPOINT=http://triplestore:8890/sparql node app.mjs
