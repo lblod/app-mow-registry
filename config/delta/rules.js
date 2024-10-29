02973ba6ade58e04b41875243fb8907cf2345c02
Nordine Bittich <contact@bittich.be>
2024-10-29 10:24:29 +0100

    tombstone service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,5 +1,22 @@
 export default [
   {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+      }
+    },
+    callback: {
+      method: 'POST',
+      url: 'http://tombstone/delta'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
+  },
+  {
     match: {},
     callback: {
       url: "http://resource/.mu/delta",
