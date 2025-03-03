14eb598a77cef484b44874ac2038b9927baf50e3
Nordine Bittich <contact@bittich.be>
2025-03-03 12:22:24 +0100

    dcat download url

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,66 +1,66 @@
 export default [
-  {
-    match: {
-      predicate: {
-        type: 'uri',
-        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
-      }
+    {
+        match: {
+            predicate: {
+                type: 'uri',
+                value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+            }
+        },
+        callback: {
+            method: 'POST',
+            url: 'http://tombstone/delta'
+        },
+        options: {
+            resourceFormat: 'v0.0.1',
+            gracePeriod: 1000,
+            ignoreFromSelf: true
+        }
     },
-    callback: {
-      method: 'POST',
-      url: 'http://tombstone/delta'
+    {
+        match: {},
+        callback: {
+            url: "http://resource/.mu/delta",
+            method: "POST",
+        },
+        options: {
+            resourceFormat: "v0.0.1",
+            gracePeriod: 250,
+            ignoreFromSelf: true,
+        },
     },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 1000,
-      ignoreFromSelf: true
-    }
-  },
-  {
-    match: {},
-    callback: {
-      url: "http://resource/.mu/delta",
-      method: "POST",
+    {
+        match: {
+            // any
+        },
+        callback: {
+            url: "http://ldes-delta-pusher/publish",
+            method: "POST",
+        },
+        options: {
+            resourceFormat: "v0.0.1",
+            ignoreFromSelf: false,
+            optOutMuScopeIds: [
+                "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
+                "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance",
+            ],
+            gracePeriod: 1000,
+        },
     },
-    options: {
-      resourceFormat: "v0.0.1",
-      gracePeriod: 250,
-      ignoreFromSelf: true,
+    {
+        match: {
+            predicate: {
+                type: 'uri',
+                value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value'
+            }
+        },
+        callback: {
+            url: "http://annotater/delta",
+            method: "POST",
+        },
+        options: {
+            resourceFormat: "v0.0.1",
+            ignoreFromSelf: true,
+            gracePeriod: 250,
+        },
     },
-  },
-  {
-    match: {
-      // any
-    },
-    callback: {
-      url: "http://ldes-delta-pusher/publish",
-      method: "POST",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      ignoreFromSelf: true,
-      optOutMuScopeIds: [
-        "http://redpencil.data.gift/id/concept/muScope/deltas/initialSync",
-        "http://redpencil.data.gift/id/concept/muScope/deltas/publicationGraphMaintenance",
-      ],
-      gracePeriod: 1000,
-    },
-  },
-  {
-    match: {
-      predicate: {
-        type: 'uri',
-        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value'
-      }
-    },
-    callback: {
-      url: "http://annotater/delta",
-      method: "POST",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      ignoreFromSelf: true,
-      gracePeriod: 250,
-    },
-  },
 ];
--- scripts/migrate-images/README.md
+++ scripts/migrate-images/README.md
@@ -7,6 +7,7 @@ This script must run only once, when v2.0 will be deployed.
 - make sure to create a backup of the virtuoso database first! That way, if the script fails, you
   can rollback
 - make the script executable: chmod a+x scripts/migrate-images/run.sh
+- run: `docker compose up -d triplestore dispatcher identifier db static-file file resource`
 - run : `mu script project-scripts migrate-images "https://register.mobiliteit.vlaanderen.be" #(change if it is not prod)`
 
 ### env variables
