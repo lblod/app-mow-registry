2483f6364bf989f3d4274bdefd8e2689beb8e6c4
Nordine Bittich <contact@bittich.be>
2025-03-05 08:50:57 +0100

    update init script

--- scripts/init-ldes-data/app.mjs
+++ scripts/init-ldes-data/app.mjs
@@ -1,44 +1,47 @@
 import { addData, getConfigFromEnv } from "@lblod/ldes-producer";
-import { rm } from 'fs/promises';
-const INPUT_GRAPH = process.env.INPUT_GRAPH || "http://mu.semte.ch/graphs/mow/registry";
+import { rm } from "fs/promises";
+const INPUT_GRAPH =
+    process.env.INPUT_GRAPH || "http://mu.semte.ch/graphs/mow/registry";
 const LDES_FOLDER = process.env.LDES_FOLDER || "ldes-mow-register";
 const LDES_FRAGMENTER = process.env.LDES_FRAGMENTER || undefined;
 
-process.env.BASE_URL = process.env.BASE_URL || "https://dev-vlag.roadsigns.lblod.info/";
+process.env.BASE_URL =
+    process.env.BASE_URL || "https://dev-vlag.roadsigns.lblod.info/";
 process.env.FOLDER_DEPTH = process.env.FOLDER_DEPTH || "1";
 process.env.PAGE_RESOURCES_COUNT = process.env.PAGE_RESOURCES_COUNT || "50";
-process.env.LDES_STREAM_PREFIX = process.env.LDES_STREAM_PREFIX || "http://data.lblod.info/streams/op/";
-process.env.TIME_TREE_RELATION_PATH = process.env.TIME_TREE_RELATION_PATH || "http://www.w3.org/ns/prov#generatedAtTime";
+process.env.LDES_STREAM_PREFIX =
+    process.env.LDES_STREAM_PREFIX || "http://data.lblod.info/streams/op/";
+process.env.TIME_TREE_RELATION_PATH =
+    process.env.TIME_TREE_RELATION_PATH ||
+    "http://www.w3.org/ns/prov#generatedAtTime";
 process.env.CACHE_SIZE = process.env.CACHE_SIZE || "10";
 process.env.DATA_FOLDER = process.env.DATA_FOLDER || "/project/data/ldes-feed";
 
 async function main() {
-  const ldesProducerConfig = getConfigFromEnv();
-  await deleteDirectory(process.env.DATA_FOLDER);
-  const count = await getTotalCount();
-  const limit = 10000;
-  const totalPages = calculatePages(count, limit);
-  console.log("count:", count, "total pages:", totalPages)
-  let triples = "";
-  for (let page = 1; page <= totalPages; page++) {
-    triples += await getGraphTriples(page, limit);
-
-  }
-  if (triples.length) {
-    await addData(ldesProducerConfig, {
-      contentType: "text/turtle",
-      folder: LDES_FOLDER,
-      body: triples,
-      fragmenter: LDES_FRAGMENTER,
-    });
-  }
-
+    const ldesProducerConfig = getConfigFromEnv();
+    await deleteDirectory(process.env.DATA_FOLDER);
+    const count = await getTotalCount();
+    const limit = 10000;
+    const totalPages = calculatePages(count, limit);
+    console.log("count:", count, "total pages:", totalPages);
+    let triples = "";
+    for (let page = 1; page <= totalPages; page++) {
+        triples += await getGraphTriples(page, limit);
+    }
+    if (triples.length) {
+        await addData(ldesProducerConfig, {
+            contentType: "text/turtle",
+            folder: LDES_FOLDER,
+            body: triples,
+            fragmenter: LDES_FRAGMENTER,
+        });
+    }
 }
 function calculatePages(totalCount, limit) {
-  return Math.ceil(totalCount / limit);
+    return Math.ceil(totalCount / limit);
 }
 async function getTotalCount() {
-  const countQuery = `
+    const countQuery = `
     PREFIX mobiliteit: <https://data.vlaanderen.be/ns/mobiliteit#>
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
     PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
@@ -86,20 +89,23 @@ async function getTotalCount() {
     }
   `;
 
-  const response = await fetch(`${process.env.MU_SPARQL_ENDPOINT}?query=${encodeURIComponent(countQuery)}`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/sparql-results+json'
-    }
-  });
+    const response = await fetch(
+        `${process.env.MU_SPARQL_ENDPOINT}?query=${encodeURIComponent(countQuery)}`,
+        {
+            method: "POST",
+            headers: {
+                Accept: "application/sparql-results+json",
+            },
+        },
+    );
 
-  const result = await response.json();
-  const count = parseInt(result.results.bindings[0].count.value, 10);
-  return count;
+    const result = await response.json();
+    const count = parseInt(result.results.bindings[0].count.value, 10);
+    return count;
 }
 async function getGraphTriples(page, limit) {
-  const offset = (page - 1) * limit;
-  const q = `
+    const offset = (page - 1) * limit;
+    const q = `
     PREFIX mobiliteit: <https://data.vlaanderen.be/ns/mobiliteit#>
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
     PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
@@ -110,7 +116,7 @@ async function getGraphTriples(page, limit) {
     PREFIX tribont: <https://w3id.org/tribont/core#>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
     PREFIX variables: <http://lblod.data.gift/vocabularies/variables/>
-
+    PREFIX qudt: <http://qudt.org/schema/qudt/>
     CONSTRUCT {?s ?p ?o} WHERE
     {
       SELECT distinct ?s ?p ?o WHERE {
@@ -139,7 +145,9 @@ async function getGraphTriples(page, limit) {
                         rdfs:Resource,
                         skos:Concept,
                         skos:ConceptScheme,
-                        tribont:Shape
+                        tribont:Shape,
+                        qudt:Unit,
+                        qudt:QuantityKind
                   ))
                }
       } ORDER BY ?s ?p ?o
@@ -150,36 +158,38 @@ async function getGraphTriples(page, limit) {
     OFFSET ${offset}
 
 `;
-  console.log(q);
+    console.log(q);
 
-  return await fetchNTriples(q);
-
-};
+    return await fetchNTriples(q);
+}
 
 async function deleteDirectory(path) {
-  try {
-    await rm(path, { recursive: true, force: true });
-  } catch (error) {
-    console.error(`Error while deleting directory ${path}:`, error);
-  }
+    try {
+        await rm(path, { recursive: true, force: true });
+    } catch (error) {
+        console.error(`Error while deleting directory ${path}:`, error);
+    }
 }
 async function fetchNTriples(query) {
-  try {
-    const response = await fetch(`${process.env.MU_SPARQL_ENDPOINT}?query=${encodeURIComponent(query)}&format=${encodeURIComponent('text/plain')}`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'text/plain'
-      }
-    });
+    try {
+        const response = await fetch(
+            `${process.env.MU_SPARQL_ENDPOINT}?query=${encodeURIComponent(query)}&format=${encodeURIComponent("text/plain")}`,
+            {
+                method: "POST",
+                headers: {
+                    Accept: "text/plain",
+                },
+            },
+        );
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return await response.text();
+    } catch (error) {
+        console.error("Error:", error);
+        process.exit(-1);
     }
-
-    return await response.text();
-  } catch (error) {
-    console.error('Error:', error);
-    process.exit(-1);
-  }
 }
-main().then()
+main().then();
