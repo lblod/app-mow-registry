a233d15654ae3d24d7891a685b873bcf4d569691
Oscar <lagartoverde97@gmail.com>
2025-03-17 14:05:02 +0100

    setup ldes autohealing

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -63,4 +63,20 @@ export default [
       gracePeriod: 250,
     },
   },
+  {
+    match: {
+      subject: {},
+    },
+    callback: {
+      url: "http://modified/delta",
+      method: "POST",
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 10000,
+      retry: 3,
+      ignoreFromSelf: true,
+      retryTimeout: 250,
+    },
+  },
 ];
