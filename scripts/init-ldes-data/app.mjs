d69361a8ddd592eb9fa899b990c8c600bf031a6b
Nordine Bittich <contact@bittich.be>
2025-03-31 13:29:21 +0200

    no need for the constant

--- scripts/init-ldes-data/app.mjs
+++ scripts/init-ldes-data/app.mjs
@@ -5,7 +5,6 @@ const INPUT_GRAPH =
 const LDES_FOLDER = process.env.LDES_FOLDER || "ldes-mow-register";
 const LDES_FRAGMENTER = process.env.LDES_FRAGMENTER || undefined;
 
-const BASE_URL = process.env.BASE_URL;
 process.env.FOLDER_DEPTH = process.env.FOLDER_DEPTH || "1";
 process.env.PAGE_RESOURCES_COUNT = process.env.PAGE_RESOURCES_COUNT || "50";
 process.env.LDES_STREAM_PREFIX =
@@ -16,7 +15,7 @@ process.env.TIME_TREE_RELATION_PATH =
 process.env.CACHE_SIZE = process.env.CACHE_SIZE || "10";
 process.env.DATA_FOLDER = process.env.DATA_FOLDER || "/project/data/ldes-feed";
 
-if (!BASE_URL?.length) {
+if (!process.env.BASE_URL?.length) {
     console.error("You must provide a BASE_URL");
     process.exit(1);
 }
