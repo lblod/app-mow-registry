496be396f889b98b25cae5b1eed55c86f3790d8a
Oscar <lagartoverde97@gmail.com>
2025-04-25 12:40:59 +0200

    add max string size config

--- config/cl-authorization/config.lisp
+++ config/cl-authorization/config.lisp
@@ -128,3 +128,8 @@
 (grant (read)
        :to-graph public
        :for-allowed-group "public")
+       
+(in-package :support)
+;; virtuoso supports strings up to 2GB in size, assuming this a character count and a char uses max 4 bytes gives us 536_870_912
+(defparameter *string-max-size* 536_870_912
+  "Maximum size of a string before it gets converted.")
